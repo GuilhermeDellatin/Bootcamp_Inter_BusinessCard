53db755d88096141ce46541c009d8c0e8c35e22d
GuilhermeDellatin <guidellatin@gmail.com>
2021-07-22 22:05:17 -0300

    Criação de Activitys, drawables, e resources de strings

--- app/src/main/java/com/gfdellatin/bootcamp_inter_businesscard/ui/MainActivity.kt
+++ app/src/main/java/com/gfdellatin/bootcamp_inter_businesscard/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.gfdellatin.bootcamp_inter_businesscard
+package com.gfdellatin.bootcamp_inter_businesscard.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gfdellatin.bootcamp_inter_businesscard.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
